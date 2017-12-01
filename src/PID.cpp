b066ba8929a31fcddc1352e3e187872507d94b06
Maxim Pechyonkin <fazzolini@gmail.com>
2017-12-01 16:43:41 +0800

    Implement PID::Init()

--- src/PID.cpp
+++ src/PID.cpp
@@ -1,3 +1,4 @@
+#include <iostream>
 #include "PID.h"
 
 using namespace std;
@@ -11,6 +12,18 @@ PID::PID() {}
 PID::~PID() {}
 
 void PID::Init(double Kp, double Ki, double Kd) {
+  // Set default time interval to 1
+  dt = 1;
+
+  // Set parameters first
+  PID::Kp = Kp;
+  PID::Ki = Ki;
+  PID::Kd = Kd;
+
+  // Initialize errors
+  p_error = 0;
+  i_error = 0;
+  d_error = 0;
 }
 
 void PID::UpdateError(double cte) {
